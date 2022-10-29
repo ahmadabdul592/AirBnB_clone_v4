9eca0a212e239e931f85c1a914bc9e2bec80a97f
Jasmin1002 <jasmineolofin@gmail.com>
2022-10-30 00:24:55 +0100

    feat: Update selection on heading

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+$(function() {
+    id = {};
+    $('input[type=checkbox]').on('change', function() {
+        if (this.checked) {
+            $(this).removeClass('checkbox').addClass('checked')
+            id[this.getAttribute('data-name')] = this.getAttribute('data-id')
+        } else {
+            $(this).removeClass('checked').addClass('checkbox')
+            for (let key in id) {
+                if (id[key] === $(this).attr('data-id')) delete id[key]
+                break;
+            }
+        }
+        const h4 = $('h4')
+        h4.text('')
+        for (let key in id) {
+            let name = key.replace('_', ' ')
+            h4.text(name)
+        }
+    })
+})
